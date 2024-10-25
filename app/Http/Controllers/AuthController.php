5ea187787b3f0d9c95c764beb9800133ef8da31d
João Vitor <jvde.olivera@gmail.com>
2024-10-25 00:28:27 -0300

    Fazendo a Segurança do Site

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function indexLogin()
+    {
+        return view('cliente.auth.login');
+    }
+
+
+    public function indexRegister()
+    {
+        return view('cliente.auth.register');
+    }
+
+    
+    public function register(Request $request)
+    {
+        // Validação dos dados
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Criação do usuário
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        // Autenticar o usuário
+        Auth::login($user);
+
+        // Redirecionar ou retornar uma resposta
+        return redirect()->route('/login')->with('success', 'Registro concluído com sucesso!');
+    }
+    public function login(Request $request)
+    {
+
+        // Validação dos dados
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // Verificar as credenciais e autenticar o usuário
+        if (Auth::attempt($credentials)) {
+            // Login bem-sucedido
+            $request->session()->regenerate();
+
+            return redirect()->intended('<perguntar pra leticia>')->with('success', 'Login bem-sucedido!');
+        }
+
+        // Se as credenciais estiverem erradas
+        return back()->withErrors([
+            'email' => 'As credenciais fornecidas estão incorretas.',
+        ]);
+    }
+}
